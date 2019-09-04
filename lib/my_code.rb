43923da9e54b37e85afdd77f9aba5b034c50310a
Warren McDonald <warrenjmac@mail.com>
2019-09-04 19:01:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@
    return new
 end
 
-def reduce(source_array, starting_point)
+def reduce(source_array, starting_point = 0)
   i = 0 
    total =  starting_point
   while i < source_array.length do
