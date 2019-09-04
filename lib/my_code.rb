ae39d805593717528a0caa33c4b5dc9e13f6988a
Warren McDonald <warrenjmac@mail.com>
2019-09-04 16:04:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
+end
