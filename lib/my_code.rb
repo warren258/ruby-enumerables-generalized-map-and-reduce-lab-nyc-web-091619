c9857739e28892ff3c6cc00e5be0dd500b64d5c9
Warren McDonald <warrenjmac@mail.com>
2019-09-04 19:51:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
  def map(source_array)
   new = []
   i = 0
-  while i < source_array.length do
+  while i < source_array.length {
     yield source_array[i]
     new.push(yield source_array[i])
     i += 1
-  end
+  }
    return new
 end
 
