db350aba365ee6e95048cc73781c0b76330e9645
Warren McDonald <warrenjmac@mail.com>
2019-09-04 20:14:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(source_array, starting_point = 0)
   # starting_point = 0
  # while i < source_array.length do
      #total = total + source_array[i]
-     yield [i]
+     yield 
      i += 1
   end
   return total
