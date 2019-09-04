c7e585325b8ba24aeee97da9f3267c6da9f70bd0
Warren McDonald <warrenjmac@mail.com>
2019-09-04 16:08:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,10 +2,11 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
+    #new.push( source_array[i] * -1 ) 
+    yield
     i += 1
   end
   return new
 end
 
-map {|| new.push( source_array[i] * -1 )}
+map {|act| new.push( source_array[i] * -1 )}
