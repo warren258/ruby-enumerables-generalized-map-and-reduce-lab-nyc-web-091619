ae16282b2f98dc8729caac53e3387806881dfc68
Warren McDonald <warrenjmac@mail.com>
2019-09-05 03:05:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,4 +9,13 @@
    return new
 end
 
- 
+ def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      return true 
+    end
+      i += 1 
+    end
+    return false
+  end
