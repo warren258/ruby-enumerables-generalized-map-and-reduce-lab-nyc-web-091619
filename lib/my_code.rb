2186ec858f3c90ac4303200a658d5bf2395cc4c2
Warren McDonald <warrenjmac@mail.com>
2019-09-05 03:56:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+ def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    yield source_array[i]
+    new.push(yield source_array[i])
+    i += 1
+  end 
+   return new
+end
+
+ def reduce(array, sp = nil)
+  if sp
+    sum = sp
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length 
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
